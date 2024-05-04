7c703a6709066f7e341586b8ba66d9d3390921c6
Praveen Kumar <praveenku32k@gmail.com>
2024-05-04 12:56:42 +0530

    llm file update

--- src/llm.py
+++ src/llm.py
@@ -2,20 +2,19 @@ import os
 from dotenv import load_dotenv
 
 from langchain_google_genai import ChatGoogleGenerativeAI
+from src.config import instruction
 
 load_dotenv()
 
 GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
 os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
 
-def ask_bot(message):
-    llm = ChatGoogleGenerativeAI(model="gemini-pro")
-    
-    respone=llm.invoke(message)
-    
-    return respone.content
+def ask_bot(user_message):
+    messages=[{"role": "system", "content":instruction }, 
+            {"role": "user", "content": user_message}]  
 
-if __name__ == "__main__":
-    print("Welcome to the chat bot!")
-    message=ask_bot("what is a meaning of large language models?")
-    print(message)
+    llm = ChatGoogleGenerativeAI(model="gemini-pro")
+        
+    respones = llm.invoke(messages)
+    
+    return respones.content
